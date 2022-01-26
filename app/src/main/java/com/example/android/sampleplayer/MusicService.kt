1258569264dc64c729b07db6579677afe9aa0604
mag0716 <mkishimoto0716@gmail.com>
2022-01-26 17:48:18 +0900

    try mediaSessionConnector.invalidateMediaSessionQueue() after set a new ShuffleOrder

--- app/src/main/java/com/example/android/sampleplayer/MusicService.kt
+++ app/src/main/java/com/example/android/sampleplayer/MusicService.kt
@@ -112,6 +112,7 @@ class MusicService : MediaBrowserServiceCompat(), LifecycleOwner {
                                         to
                                     )
                                 )
+                                mediaSessionConnector.invalidateMediaSessionQueue()
                             }
                         }
                     },
